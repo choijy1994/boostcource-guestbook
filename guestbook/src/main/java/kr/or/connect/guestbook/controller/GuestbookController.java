ef92e6e3b0fae46df5f4f1db172807681112b2ec
Choijy <shvk54@gmail.com>
2020-06-21 13:54:29 +0900

    boostcourse-guestbook cookie and session

--- guestbook/src/main/java/kr/or/connect/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/kr/or/connect/guestbook/controller/GuestbookController.java
@@ -3,7 +3,9 @@ package kr.or.connect.guestbook.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -23,21 +25,44 @@ public class GuestbookController {
 	
 	@GetMapping(path="/list")
 	public String list(@RequestParam(name="start", required=false, defaultValue="0") int start,
-					   ModelMap model) {
+					   ModelMap model,HttpServletRequest request,
+					   HttpServletResponse response) {
 		
-		// start로 시작하는 방명록 목록 구하기
+		String value = null;
+		boolean find = false;
+		Cookie[] cookies = request.getCookies();
+		if(cookies!=null) {
+			for(Cookie cookie:cookies) {
+				if("count".equals(cookie.getName())) {
+					find = true;
+					value = cookie.getValue();
+					break;
+				}
+			}
+		}
+		
+		if(!find) {
+			value = "1";
+		}else {
+			try {
+				int i = Integer.parseInt(value);
+				value = Integer.toString(++i);
+			} catch (Exception e) {
+				value="1";
+			}
+		}
+		Cookie cookie = new Cookie("count",value);
+		cookie.setMaxAge(60*60*24*365);
+		cookie.setPath("/");
+		response.addCookie(cookie);
+		
 		List<Guestbook> list = guestbookService.getGuestbooks(start);
 		
-		// 전체 페이지수 구하기
 		int count = guestbookService.getCount();
 		int pageCount = count / GuestbookService.LIMIT;
 		if(count % GuestbookService.LIMIT > 0)
 			pageCount++;
 		
-		// 페이지 수만큼 start의 값을 리스트로 저장
-		// 예를 들면 페이지수가 3이면
-		// 0, 5, 10 이렇게 저장된다.
-		// list?start=0 , list?start=5, list?start=10 으로 링크가 걸린다.
 		List<Integer> pageStartList = new ArrayList<>();
 		for(int i = 0; i < pageCount; i++) {
 			pageStartList.add(i * GuestbookService.LIMIT);
@@ -46,7 +71,7 @@ public class GuestbookController {
 		model.addAttribute("list", list);
 		model.addAttribute("count", count);
 		model.addAttribute("pageStartList", pageStartList);
-		
+		model.addAttribute("cookieCount", value);
 		return "list";
 	}
 	
